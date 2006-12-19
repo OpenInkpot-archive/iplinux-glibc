c41aea2e8988dae874519bbe2721aee72922edbe
Alexander Shishkin <alexander.shishkin@siemens.com>
2006-12-19 19:29:55 +0300

    sysdeps/arm.mk: added ports to the list of add-ons Since glibc 2.4 arm lives off the main tree, in 'ports' addon. Signed-off-by: Alexander Shishkin <alexander.shishkin@siemens.com>

--- debian/sysdeps/arm.mk
+++ debian/sysdeps/arm.mk
@@ -1,4 +1,4 @@
-#libc_add-ons = linuxthreads $(add-ons)
+libc_add-ons = linuxthreads ports $(add-ons)
 libc_extra_config_options = $(extra_config_options) --disable-sanity-checks --without-tls --without-__thread
 #libc-headers_add-ons = linuxthreads $(add-ons)
 #with_fp := --without-fp
