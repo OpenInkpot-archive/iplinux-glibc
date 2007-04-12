b9f22e52f4ab5ab315ce1a38cf21a13d726cca18
Alexander Shishkin <virtuoso@slind.org>
2007-04-12 12:50:48 +0400

    sh4: basic support * added a patch to turn off -Wl,-z,defs where unneeded * added arch configuration file (debian/sysdeps/sh4.mk) Signed-off-by: Alexander Shishkin <virtuoso@slind.org>

---
+++ debian/sysdeps/sh4.mk
@@ -0,0 +1,6 @@
+libc_add-ons = nptl $(add-ons)
+libc_extra_config_options = $(extra_config_options) --disable-sanity-checks
+# --without-tls --without-__thread
+#libc-headers_add-ons = linuxthreads $(add-ons)
+#with_fp := --with-fp
+
