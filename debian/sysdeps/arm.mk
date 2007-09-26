2a4c0a636fe63e26d4ea8613f879306068e5978f
Alexander Shishkin <virtuoso@slind.org>
2007-09-26 16:00:59 +0400

    arm: use nptl Signed-off-by: Alexander Shishkin <virtuoso@slind.org>

--- debian/sysdeps/arm.mk
+++ debian/sysdeps/arm.mk
@@ -1,5 +1,5 @@
-libc_add-ons = linuxthreads ports $(add-ons)
-libc_extra_config_options = $(extra_config_options) --disable-sanity-checks --without-tls --without-__thread
-#libc-headers_add-ons = linuxthreads $(add-ons)
+libc_add-ons = nptl ports $(add-ons)
+libc_extra_config_options = $(extra_config_options) --disable-sanity-checks
+libc-headers_add-ons = nptl ports $(add-ons)
 #with_fp := --without-fp
 
