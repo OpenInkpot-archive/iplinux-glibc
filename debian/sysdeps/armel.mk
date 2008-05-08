d96f4be1b3f80cd46784ae76726b2276ab9cb8dc
Mikhail Gusarov <dottedmag@dottedmag.net>
2008-05-08 21:56:08 +0700

    Fixed armel.mk - libc_extra_options explicitly defined

--- debian/sysdeps/armel.mk
+++ debian/sysdeps/armel.mk
@@ -1,3 +1,4 @@
 libc_add-ons = nptl ports $(add-ons)
 libc_MIN_KERNEL_SUPPORTED=2.6.14
+libc_extra_config_options = $(extra_config_options) --disable-sanity-checks
 libc-headers_add-ons = nptl ports $(add-ons)
