c4adce7a15716081baf32419ba3af424a1959ca0
dottedmag <Mikhail Gusarov dottedmag@dottedmag.net>
2009-02-07 03:11:58 +0600

    Cosmetic: expanded comment in armel.mk

--- debian/sysdeps/armel.mk
+++ debian/sysdeps/armel.mk
@@ -12,6 +12,7 @@ libc_MIN_KERNEL_SUPPORTED = 2.6.14
 # Some tests assume a fast machine
 TIMEOUTFACTOR=3
 
-# bootstrap needs this macro
-libc-headers_extra_cflags = -D__ARM_EABI__
+# bootstrap needs this macro. It is expected to be defined by the compiler on
+# later stages.
 
+libc-headers_extra_cflags = -D__ARM_EABI__
