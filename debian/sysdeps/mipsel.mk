884c52d1dd37468b2d6c9e26ed4388bd40b6f6ed
Mikhail Gusarov <dottedmag@dottedmag.net>
2009-04-02 04:53:58 +0700

    mipsel is softfp now, pass --without-fp manually.

--- debian/sysdeps/mipsel.mk
+++ debian/sysdeps/mipsel.mk
@@ -6,6 +6,10 @@ libc_configure-args = libc_cv_mips_tls=yes
 libc-second_configure-args = $(libc_configure-args)
 libc-headers_configure-args = $(libc_configure-args)
 
+# glibc can't detect softfp from the GCC configuration. Oh well...
+# See the http://sourceware.org/ml/libc-ports/2007-06/msg00008.html
+extra_config_options = --without-fp
+
 # Some tests assume a fast machine
 TIMEOUTFACTOR=2
 
